582c37a7409e1b26fd8de13acb39f0a6fa8a88b0
Eleazar Ramos Cortés <sixen25@gmail.com>
2022-01-30 23:47:29 +0100

    Inicio Mapas v2

--- Angular 1 - 12 - Auth-Server/index.js
+++ Angular 1 - 12 - Auth-Server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const { dbConnection } = require('./db/config');
 require('dotenv').config();
 /* console.log( process.env ); */
@@ -21,6 +22,13 @@ app.use( express.json() );
 
 // Rutas
 app.use('/api/auth', require('./routes/auth'));
+
+
+// Manejador de rutas (despliegue) para redirigir a Angular
+app.get( '*' , (req, res) => {
+    res.sendFile( path.resolve( __dirname, 'public/index.html'));
+})
+
 
 app.listen( process.env.PORT, () => {
     console.log(`Servidor corriendo en puerto ${ process.env.PORT }`)
